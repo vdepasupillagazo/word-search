6a00c97e605076d2a526284a3689adb8c39ce0d8
Victoria Depasupil Lagazo <jgdepasupillagazo@up.edu.ph>
2024-11-19 20:55:02 +0800

    word-search.py

---
+++ word-search.py
@@ -0,0 +1,54 @@
+################################################################
+# Code for our version of the game Word Search (a.k.a. Boggle) #
+# Written by Group 6 for CMSC 202, 1st Sem AY 2024-2025        #
+################################################################
+
+def selectGridSize():
+    # print grid size options and validate user input
+    # only menu options should be accepted
+    return
+
+def selectTimerOption():
+    # print timer options and validate user input
+    # only menu options should be accepted
+    return
+
+def loadWordLibrary():
+    # read words from file and return as list for use in program
+    return
+
+def generateGrid():
+    # generate empty tile grid prepopulated with placeholder characters
+    return
+
+def timer():
+    # handles logic for timer
+    return
+
+def randomizer():
+    # generate random list of letters to use as game tiles
+    return
+
+def generateWordList():
+    # compiles the list of words that the user can find in the game
+    return
+
+def printTiles():
+    # prints the game tiles
+    return
+
+def scoreWord():
+    # assigns a score to each word found, depending on the length
+    return
+
+def checkWord():
+    # checks if user found a correct word
+    return
+
+def printWordList():
+    # prints all possible words to be found at game end
+    return
+
+def wordSearch():
+    # game start!
+    return
