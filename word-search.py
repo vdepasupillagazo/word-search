e3a838ccab82e11f38e11a1025c3b85fd8a685ba
Patrick Cuartero <pgcuartero@up.edu.ph>
2024-12-05 19:50:40 +0800

    Replaced the word "exit" to quit the game to numerical value 0.

--- word-search.py
+++ word-search.py
@@ -243,9 +243,9 @@ def word_search():
     
     currentScore = 0  
     while True:  
-        wordInput = input('\nEnter word (or type "exit" to quit): ')
+        wordInput = input('\nEnter word (or type "0" to quit): ')
         key = wordInput[:3]  
-        if wordInput.lower() == "exit":
+        if wordInput.lower() == "0": 
             print("\nThank you for playing!")
             break
         if key in valid_words:
